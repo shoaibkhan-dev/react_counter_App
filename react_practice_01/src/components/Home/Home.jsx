668e25b5baefdfe66484bc90d19c269e887dfb87
Shoaib khan <shoaibmangal@yahoo.com>
2025-08-21 20:37:04 +0500

    add home page

--- react_practice_01/src/components/Home/Home.jsx
+++ react_practice_01/src/components/Home/Home.jsx
@@ -1,1 +1,9 @@
+function Home(){
+    return 
+    <>
+     <h1>This is home page</h1>
+     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo in consequatur facilis. Consequuntur reprehenderit voluptatum iusto possimus inventore, aperiam laborum magnam consectetur placeat hic facere deserunt eum, sint, blanditiis eius.</p>
+    </>
+}
+
 export default Home
