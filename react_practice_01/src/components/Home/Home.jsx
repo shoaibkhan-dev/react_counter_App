d6d24909d6a83221dc158f9aaa126d87d5b1a310
Shoaib khan <shoaibmangal@yahoo.com>
2025-08-22 23:51:38 +0500

    add paragraph

--- react_practice_01/src/components/Home/Home.jsx
+++ react_practice_01/src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ function Home(){
      <h1>This is home page</h1>
      <h2>Home page</h2>
      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo in consequatur facilis. Consequuntur reprehenderit voluptatum iusto possimus inventore, aperiam laborum magnam consectetur placeat hic facere deserunt eum, sint, blanditiis eius.</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, veniam sapiente? Necessitatibus vel fuga iure quaerat? Nisi qui enim porro.</p>
     </>
 }
 
