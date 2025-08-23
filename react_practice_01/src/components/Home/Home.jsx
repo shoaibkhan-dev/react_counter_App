811e6c91dc44220d17e7b0b993956877cdc9dec4
Shoaib khan <shoaibmangal@yahoo.com>
2025-08-23 23:47:54 +0500

    add form in home.jsx file

--- react_practice_01/src/components/Home/Home.jsx
+++ react_practice_01/src/components/Home/Home.jsx
@@ -5,6 +5,9 @@ function Home(){
      <h2>Home page</h2>
      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo in consequatur facilis. Consequuntur reprehenderit voluptatum iusto possimus inventore, aperiam laborum magnam consectetur placeat hic facere deserunt eum, sint, blanditiis eius.</p>
      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, veniam sapiente? Necessitatibus vel fuga iure quaerat? Nisi qui enim porro.</p>
+     <form action="">
+        <input type="text" />
+     </form>
     </>
 }
 
